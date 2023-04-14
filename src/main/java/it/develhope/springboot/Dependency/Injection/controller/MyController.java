2a17b0bcf560d4a6df4f45aeb58143daeb1282dd
Alessio Limina <alessio.limina@gmail.com>
2023-04-14 17:45:06 +0200

    Exercise Dependency Injection updated, to be completed

--- src/main/java/it/develhope/springboot/Dependency/Injection/controller/MyController.java
+++ src/main/java/it/develhope/springboot/Dependency/Injection/controller/MyController.java
@@ -1,6 +1,7 @@
 package it.develhope.springboot.Dependency.Injection.controller;
 
 import it.develhope.springboot.Dependency.Injection.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MyController {
 
+    @Autowired
     private MyService myService;
 
     public MyController(MyService myService) {
--- src/main/java/it/develhope/springboot/Dependency/Injection/service/MyService.java
+++ src/main/java/it/develhope/springboot/Dependency/Injection/service/MyService.java
@@ -2,10 +2,12 @@ package it.develhope.springboot.Dependency.Injection.service;
 
 import it.develhope.springboot.Dependency.Injection.component.MyComponent;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
+    @Autowired
     private MyComponent myComponent;
 
     public MyService(MyComponent myComponent){
